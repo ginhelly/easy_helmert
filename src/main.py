e512ab293f116da387673b2d8a8cc18cf211be9f
karav-n <karav-n@mail.ru>
2026-03-13 20:57:26 +0400

    bundle

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 import wx
 from gui.controllers.main_frame import MainFrame
 
+import os
+import sys
+
+if hasattr(sys, "_MEIPASS"):
+    os.environ["GDAL_DATA"] = os.path.join(
+        sys._MEIPASS, "rasterio", "gdal_data"
+    )
+    os.environ["PROJ_DATA"] = os.path.join(
+        sys._MEIPASS, "pyproj", "proj_dir", "share", "proj"
+    )
+    os.environ["PROJ_LIB"]      = os.environ["PROJ_DATA"]
+    os.environ["RASTERIO_DATA"] = os.environ["GDAL_DATA"]
+
 def main():
     app = wx.App(False)
     frame = MainFrame()
